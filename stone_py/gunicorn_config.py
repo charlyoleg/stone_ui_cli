8c43608c620c03096747004acfd0621cf5ef8e49
charli <charlyoleg@fabfolk.com>
2019-04-05 21:44:46 +0200

    renaming key and certificate

--- stone_py/gunicorn_config.py
+++ stone_py/gunicorn_config.py
@@ -1,8 +1,8 @@
 # gunicorn_config.py
 # configuration file for gunicorn
 
-certfile = 'server.crt'
-keyfile = 'server.key'
+certfile = 'gunicorn_server.crt'
+keyfile = 'gunicorn_server.key'
 
 bind = '0.0.0.0:8443'
 
