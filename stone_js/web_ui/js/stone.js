f4fd0d0c79a3ba15b87d9fe7b61a1f2c7782fdab
charli <charlyoleg@fabfolk.com>
2019-04-27 21:01:25 +0200

    enhance coding style

--- stone_js/web_ui/js/stone.js
+++ stone_js/web_ui/js/stone.js
@@ -6,30 +6,31 @@
 // ####################################
 // helper stuff
 // ####################################
+
 class CallStatistics {
-    constructor(){
+    constructor() {
         this.brick_height = 0;
         this.wall_thermal_conductivity = 0;
     }
 
-    tick_off_brick_weight(){
+    tick_off_brick_weight() {
         this.brick_height += 1;
     }
 
-    tick_off_wall_thermal_conductivity(){
+    tick_off_wall_thermal_conductivity() {
         this.wall_thermal_conductivity += 1;
     }
 
-    get_statistics(){
+    get_statistics() {
         //let brick_height_str = this.brick_height.toString();
         //let wall_thermal_conductivity_str = this.wall_thermal_conductivity.toString();
         let total = this.brick_height + this.wall_thermal_conductivity;
         //let total_str = total.toString();
         let r = "visit counts:\n";
         r += `brick_weight calls                : ${this.brick_height}\n`;
-        r += `wall_thermal_conductivity calls   : ${this.wall_thermal_conductivity}\n`
-        r += `total calls                       : ${total}\n`
-        return(r);
+        r += `wall_thermal_conductivity calls   : ${this.wall_thermal_conductivity}\n`;
+        r += `total calls                       : ${total}\n`;
+        return r;
     }
 }
 
@@ -41,26 +42,26 @@ const call_stat = new CallStatistics();
 // the stone core
 // ####################################
 
-function stone_weight(width, height, thickness, density){
-    console.log('stone_core: stone_weight: width: '+ width);
+function stone_weight(width, height, thickness, density) {
+    console.log('stone_core: stone_weight: width: ' + width);
     call_stat.tick_off_brick_weight();
     console.log("W: " + width.toFixed(2) + "  H: " + height.toFixed(2) +
         "  T: " + thickness.toFixed(2) + "  D: " + density.toFixed(2));
     let r_weight = width * height * thickness * density;
-    return(r_weight);
+    return r_weight;
 }
 
-function wall_thermal_conductivity(conductivity, thickness){
-    console.log('stone_core: wall_thermal_conductivity: conductivity: '+ conductivity.toFixed(2));
+function wall_thermal_conductivity(conductivity, thickness) {
+    console.log('stone_core: wall_thermal_conductivity: conductivity: ' + conductivity.toFixed(2));
     call_stat.tick_off_wall_thermal_conductivity();
     let r_wall_conductivity = conductivity / thickness;
-    return(r_wall_conductivity);
+    return r_wall_conductivity;
 }
 
 // the sugar
-function call_activities(){
+function call_activities() {
     console.log('stone_core: call_activities');
-    return({visit_stat: call_stat.get_statistics()});
+    return { visit_stat: call_stat.get_statistics() };
 }
 
 
