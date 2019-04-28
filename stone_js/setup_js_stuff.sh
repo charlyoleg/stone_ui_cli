b9e930ee3680ea213476e0ba2c3389ab731b1dcb
charli <charlyoleg@fabfolk.com>
2019-04-28 21:17:29 +0200

    script to install js packages

---
+++ stone_js/setup_js_stuff.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# setup_js_stuff.sh
+
+# move to the script directory
+cd $(dirname $0)
+
+echo "Prepare the javascript environment"
+
+npm install
+
+echo "End of setup_js_stuff.sh"
+
