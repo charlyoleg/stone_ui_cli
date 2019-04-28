8642497f79bd68f2c520849ac4491bb2d994ff51
charli <charlyoleg@fabfolk.com>
2019-04-28 20:01:17 +0200

    stone.js using an other sub-es6-module

--- stone_js/web_ui/js/stone.js
+++ stone_js/web_ui/js/stone.js
@@ -2,40 +2,14 @@
 
 'use strict';
 
+import * as stone_sub from './stone_sub.js';
 
 // ####################################
 // helper stuff
 // ####################################
 
-class CallStatistics {
-    constructor() {
-        this.brick_height = 0;
-        this.wall_thermal_conductivity = 0;
-    }
 
-    tick_off_brick_weight() {
-        this.brick_height += 1;
-    }
-
-    tick_off_wall_thermal_conductivity() {
-        this.wall_thermal_conductivity += 1;
-    }
-
-    get_statistics() {
-        //let brick_height_str = this.brick_height.toString();
-        //let wall_thermal_conductivity_str = this.wall_thermal_conductivity.toString();
-        let total = this.brick_height + this.wall_thermal_conductivity;
-        //let total_str = total.toString();
-        let r = "visit counts:\n";
-        r += `brick_weight calls                : ${this.brick_height}\n`;
-        r += `wall_thermal_conductivity calls   : ${this.wall_thermal_conductivity}\n`;
-        r += `total calls                       : ${total}\n`;
-        return r;
-    }
-}
-
-
-const call_stat = new CallStatistics();
+const call_stat = new stone_sub.CallStatistics();
 
 
 // ####################################
