14fcb49a4eb007c606df9b71655c791461fb06d4
charli <charlyoleg@fabfolk.com>
2019-04-28 20:07:18 +0200

    more comment about esm and es6-module used in node

--- stone_js/user_prog.js
+++ stone_js/user_prog.js
@@ -2,6 +2,7 @@
 
 'use strict';
 
+// A black-magic solution from ESM, waiting for the official support for es6-module by nodejs, planned with node-V12.0
 require = require("esm")(module/*, options*/);
 const stone = require("./web_ui/js/stone.js");
 
