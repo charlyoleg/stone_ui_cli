554a9854d1b557a298f74c834e33bcfefad125b9
charli <charlyoleg@fabfolk.com>
2019-04-28 21:37:31 +0200

    complete micro-service stone.js

---
+++ stone_js/run_network_api_micro_service.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# run_network_api_micro_service.sh
+
+cd $(dirname $0)
+
+# start the REST API server in background
+echo "Start the micro-service server ..."
+gnome-terminal --working-directory=$(pwd) -- bash -c 'node stone_rest_app.js' 
+# give time to the server to start its service
+sleep 2
+
+
+
+# one request from the browser
+MY_URL="https://localhost:8443/stone_weight?width=0.4&height=0.2&thickness=0.2&density=2800"
+#sensible-browser "${MY_URL}"
+echo -e "1st curl requests:\ncurl -X GET -k '${MY_URL}'"
+curl -X GET -k "${MY_URL}"
+echo -e "\n"
+
+MY_URL="https://localhost:8443/stone_weight?width=0.4&height=0.2&thickness=0.4&density=2800"
+echo -e "2nd curl requests:\ncurl -X GET -k '${MY_URL}'"
+curl -X GET -k "${MY_URL}"
+echo -e "\n"
+
+MY_URL="https://localhost:8443/wall_thermal_conductivity?thickness=0.4&conductivity=3.1"
+echo -e "3rd curl requests:\ncurl -X GET -k '${MY_URL}'"
+curl -X GET -k "${MY_URL}"
+echo -e "\n"
+
+MY_URL="https://localhost:8443/wall_thermal_conductivity?thickness=0.2&conductivity=3.1"
+echo -e "4th curl requests:\ncurl -X GET -k '${MY_URL}'"
+curl -X GET -k "${MY_URL}"
+echo -e "\n"
+
+MY_URL="https://localhost:8443/call_activities"
+echo -e "5th curl requests:\ncurl -X GET -k '${MY_URL}'"
+curl -X GET -k "${MY_URL}"
+echo -e "\n"
+
+
+
+# stop the server
+echo "Stop the micro-service server ..."
+#ps aux | grep stone_rest_app.js | grep node | awk '{ print $2; }' | xargs -n 1 kill
+echo "Check remaining process ..."
+ps aux | grep node
+
